219e585c320c894db243bc8962169c8a37d27fba
raylopez <raylopez@192.168.21.45>
2023-10-10 14:56:36 -0400

    added SOS function
    in class assignment

--- SOSarduino.ino
+++ SOSarduino.ino
@@ -1,24 +1,35 @@
+#include <Servo.h>
+
+Servo myServo;
+
 void setup() {
   // put your setup code here, to run once:
  pinMode(LED_BUILTIN,OUTPUT);
+ myServo.attach(11);
+ myServo.write(90);
 
 }
 
 void blinkOnce(int time){
    digitalWrite(LED_BUILTIN, HIGH);
+   myServo.write(130);
  delay(time);
  digitalWrite(LED_BUILTIN, LOW);
+ myServo.write(50);
  delay(time);
 }
 
-void loop() {
- blinkOnce(1000);
- blinkOnce(1000);
- blinkOnce(1000);
- blinkOnce(500);
- blinkOnce(500);
- blinkOnce(500);
- blinkOnce(1000);
- blinkOnce(1000);
- blinkOnce(1000);
+void BlinkSOS() {
+ blinkOnce(700);
+ blinkOnce(700);
+ blinkOnce(700);
+ blinkOnce(300);
+ blinkOnce(300);
+ blinkOnce(300);
+ blinkOnce(700);
+ blinkOnce(700);
+ blinkOnce(700);
 } 
+void loop(){
+  BlinkSOS();
+}
